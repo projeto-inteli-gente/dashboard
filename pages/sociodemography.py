90136a29e7251e8944b25a8a5955b68116cef7c6
g-faccini <gabriel_faccini@hotmail.com>
2024-10-10 02:43:40 -0300

    Adicionado imagem para grafico de nivel de maturidade

--- pages/sociodemography.py
+++ pages/sociodemography.py
@@ -19,7 +19,7 @@ col1, col2 = st.columns(spec=[1, 4], gap="medium")
 with col1:
 
     # TODO fazer imagens para cada nivel e acessar o back para recuperar o nivel
-    st.image('images/circle.png')
+    st.image('images/grafico_niveis/grafico_nivel05.png')
 
     add_vertical_space(2)
 
